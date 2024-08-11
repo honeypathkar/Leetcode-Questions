adfc94eaefe26dae524f4124fa42a55fd4f9704e
honeypathkar <honeypatkar70@gmail.com>
2024-08-11 12:25:30 +0530

    Que. 2824. Count Pairs Whose Sum is Less than Target

---
+++ Array/countPairs.js
@@ -0,0 +1,27 @@
+/* 2824. Count Pairs Whose Sum is Less than Target
+Acceptance Rate: 87.4% */
+
+/* Test Case:
+input: nums = [-1,1,2,3,1], target = 2
+Expected output: 3
+Explanation: There are 3 pairs of indices that satisfy the conditions in the statement:
+- (0, 1) since 0 < 1 and nums[0] + nums[1] = 0 < target
+- (0, 2) since 0 < 2 and nums[0] + nums[2] = 1 < target 
+- (0, 4) since 0 < 4 and nums[0] + nums[4] = 0 < target
+Note that (0, 3) is not counted since nums[0] + nums[3] is not strictly less than the target. */
+
+var countPairs = function (nums, target) {
+  let count = 0;
+  let n = nums.length;
+  for (let i = 0; i < n - 1; i++) {
+    for (let j = i + 1; j < n; j++) {
+      if (nums[i] + nums[j] < target) {
+        count++;
+      }
+    }
+  }
+  return count;
+};
+
+console.log(countPairs([-1, 1, 2, 3, 1], 2)); // 3
+console.log(countPairs([-6, 2, 5, -2, -7, -1, 3], -2)); // 10
