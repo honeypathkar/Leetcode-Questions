e5e65450e45d7400280c3673f9a1351bc828fe10
honeypathkar <honeypatkar70@gmail.com>
2024-10-21 12:36:37 +0530

    Que. 1672. Richest Customer Wealth

---
+++ Array/maximumWealth.js
@@ -0,0 +1,36 @@
+/* 1672. Richest Customer Wealth
+Acceptance Rate: 88.3% */
+
+/* Test Case:
+input: accounts = [[1,2,3],[3,2,1]]
+Expected output: 6
+Explanation:
+1st customer has wealth = 1 + 2 + 3 = 6
+2nd customer has wealth = 3 + 2 + 1 = 6
+Both customers are considered the richest with a wealth of 6 each, so return 6. */
+
+var maximumWealth = function (accounts) {
+  let sumArray = [];
+  for (let i = 0; i < accounts.length; i++) {
+    let sum = 0;
+    for (let j = 0; j < accounts[i].length; j++) {
+      sum += accounts[i][j];
+    }
+    sumArray.push(sum);
+  }
+  return Math.max(...sumArray);
+};
+
+console.log(
+  maximumWealth([
+    [1, 2, 3],
+    [3, 2, 1],
+  ])
+);
+console.log(
+  maximumWealth([
+    [1, 5],
+    [7, 3],
+    [3, 5],
+  ])
+);
