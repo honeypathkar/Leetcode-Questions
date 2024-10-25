9c75ee29da5151c874cfafc6e34893fabfcb6456
honeypathkar <honeypatkar70@gmail.com>
2024-10-25 13:02:13 +0530

    Que. 344. Reverse String 1

---
+++ String/reverseString1.js
@@ -0,0 +1,13 @@
+/* 344. Reverse String 1
+Acceptance Rate: 79.1% */
+
+/* Test Case:
+Input: s = ["h","e","l","l","o"]
+Output: ["o","l","l","e","h"] */
+
+var reverseString1 = function (s) {
+  return s.reverse();
+};
+
+console.log(reverseString1(["h", "e", "l", "l", "o"]));
+console.log(reverseString1(["H", "a", "n", "n", "a", "h"]));
