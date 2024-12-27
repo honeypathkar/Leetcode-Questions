c5926bb8bfad4cec7ca2818389c870b941aa66ee
honeypathkar <honeypatkar70@gmail.com>
2024-12-27 16:07:53 +0530

    Que. 704. Binary Search

---
+++ Array/binarySearch.js
@@ -0,0 +1,27 @@
+/* 704. Binary Search
+Acceptance Rate: 58.8% */
+
+/* Test Case:
+input: nums = [-1,0,3,5,9,12], target = 9
+Expected output: 4
+Explanation: 9 exists in nums and its index is 4 */
+
+var search = function (nums, target) {
+  let start = 0;
+  let end = nums.length - 1;
+  while (start <= end) {
+    let mid = Math.floor((start + end) / 2);
+
+    if (nums[mid] === target) {
+      return mid;
+    } else if (nums[mid] < target) {
+      start = mid + 1;
+    } else {
+      end = mid - 1;
+    }
+  }
+  return -1;
+};
+
+console.log(search([-1, 0, 3, 5, 9, 12], 9));
+console.log(search([-1, 0, 3, 5, 9, 12], 2));
